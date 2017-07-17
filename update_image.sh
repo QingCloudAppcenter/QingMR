d4360b605a58a4b15f07311230d363078e084460
Benjamin Huo <bingjiehuo@yunify.com>
2017-07-17 21:10:45 +0800

    update image

--- update_image.sh
+++ update_image.sh
@@ -3,14 +3,6 @@ role=$1
 if [ $# -lt 1 ];then
     echo error!
 else
-    cp -r confd/$role/conf.d /etc/confd/
-    cp -r confd/$role/templates /etc/confd/
-    cp -r scripts/hadoop/sbin /opt/hadoop/
-    cp -r scripts/spark/sbin /opt/spark/
-    cp jars/S3/* /opt/hadoop/share/hadoop/common/lib/
-    if [ "x$role" = "xbigdata-client" ];then
-    	cp jars/mysql-connector-java-5.1.39-bin.jar /opt/hive/lib/
-    fi
     #tar -xvzf hadoop-2.7.3-build.tar.gz -C /opt/
     #rm /opt/hadoop
     #ln -s /opt/hadoop-2.7.3 /opt/hadoop
@@ -19,4 +11,12 @@ else
     #ln -s /opt/spark-2.2.0-bin-hadoop2.7 /opt/spark
     #cp /opt/spark/conf/spark-defaults.conf.template /opt/spark/conf/spark-defaults.conf
     #chown -R root:root /opt/
+    cp -r confd/$role/conf.d /etc/confd/
+    cp -r confd/$role/templates /etc/confd/
+    cp -r scripts/hadoop/sbin /opt/hadoop/
+    cp -r scripts/spark/sbin /opt/spark/
+    cp jars/S3/* /opt/hadoop/share/hadoop/common/lib/
+    if [ "x$role" = "xbigdata-client" ];then
+    	cp jars/mysql-connector-java-5.1.39-bin.jar /opt/hive/lib/
+    fi
 fi
