b646936b5454c45075b9f7347fa63999b67858b3
Benjamin Huo <bingjiehuo@yunify.com>
2017-07-24 21:59:22 +0800

    force copy scheduler file in case they're occupied by process

--- scripts/hadoop/sbin/copy-custom-files.sh
+++ scripts/hadoop/sbin/copy-custom-files.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 export JAVA_HOME=/usr/jdk
-USER=root /opt/hadoop/bin/hdfs dfs -get /tmp/hadoop-yarn/*-scheduler.xml /opt/hadoop/etc/hadoop/
+USER=root /opt/hadoop/bin/hdfs dfs -get /tmp/hadoop-yarn/*-scheduler.xml /opt/hadoop/etc/
+USER=root mv -f /opt/hadoop/etc/*-scheduler.xml /opt/hadoop/etc/hadoop/
 exit 0
