5734a249e47b51c6768cc2091b835384314a8356
Benjamin <benjamin@yunify.com>
2018-01-16 18:48:00 +0800

    change hive stop script

--- scripts/hive/sbin/stop-hiveserver2.sh
+++ scripts/hive/sbin/stop-hiveserver2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kill `ps ax | grep HiveServer2 | grep -v grep | awk '{print $1}'` &>/dev/null
+kill -9 `ps ax | grep HiveServer2 | grep -v grep | awk '{print $1}'` &>/dev/null
 exit 0
--- scripts/hive/sbin/stop-metastore.sh
+++ scripts/hive/sbin/stop-metastore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kill `ps ax | grep HiveMetaStore | grep -v grep | awk '{print $1}'` &>/dev/null
+kill -9 `ps ax | grep HiveMetaStore | grep -v grep | awk '{print $1}'` &>/dev/null
 exit 0
