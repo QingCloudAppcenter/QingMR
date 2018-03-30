dad1f7e71c828b3a9c72ca912188a2d10ffafc73
johuo <johuo@yunify.com>
2018-03-30 22:48:40 +0800

    update for kylin

--- scripts/kylin/sbin/action-kylin.sh
+++ scripts/kylin/sbin/action-kylin.sh
@@ -11,57 +11,79 @@ echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - add /root/ignore_he
 
 
 if [ "$action"x == "start"x ]
-then
-	/opt/kap-plus/sbin/start-kylin.sh   
+then 
+	/opt/kap-plus/sbin/j-start-kylin.sh   
 	if [  $? -ne 0 ] 
 	then 
-		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - Error -exit code=$?, Start Kylin Service failed." 1>>$KYLINAPP_LOG  2>&1	 
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - Error -exit code=$?, Start Kylin Service failed." 1>>$KYLINAPP_LOG  2>&1	
+		rm /root/ignore_healthcheck
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - rm /root/ignore_healthcheck to recovery appcenter healthcheck. " 1>>$KYLINAPP_LOG  2>&1  
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - Action=$action End ...." 1>>$KYLINAPP_LOG  2>&1 
 		exit 1  
 	fi
 	
 	if [ ! -f "/opt/kap-plus/sbin/hdfsfolder_created" ]
-	then
-		#echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - DealWithHDFS4Kylin Start. " 1>>$KYLINAPP_LOG  2>&1
-		$(DealWithHDFS4Kylin)
-		#echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - DealWithHDFS4Kylin End. " 1>>$KYLINAPP_LOG  2>&1 
+	then 
+		$(DealWithHDFS4Kylin) 
 	fi
 	
 	if [ ! -f "/opt/kap-plus/sbin/sample_loaded" ]
-	then
-		#echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - loadSampleData4Kylin Start. " 1>>$KYLINAPP_LOG  2>&1
-		$(loadSampleData4Kylin)
-		#echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - loadSampleData4Kylin End. " 1>>$KYLINAPP_LOG  2>&1 
+	then 
+		$(loadSampleData4Kylin) 
 	fi
+	
+	if [  -f "/opt/kap-plus/sbin/neverStartFlag" ]
+	then 
+		rm /opt/kap-plus/sbin/neverStartFlag
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - After cluster init, remove the neverStartFlag when start service." 1>>$KYLINAPP_LOG  2>&1
+	fi  
 fi
 
 if [ "$action"x == "stop"x ]
-then 
+then 	
 	/opt/kap-plus/sbin/stop-kylin.sh
-    echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - Kylin Service already Stopped." 1>>$KYLINAPP_LOG  2>&1 	 
+    echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - Kylin Service already Stopped." 1>>$KYLINAPP_LOG  2>&1  
 fi
 
 if [ "$action"x == "restart"x ]
-then
-	echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - stop-kylin.sh." 1>>$KYLINAPP_LOG  2>&1  
+then 
+	pid=`ps ax | grep kylin | grep -v grep | grep -v 'su kylin' | grep -v 'bash' | grep 'Dkylin.hive.dependency' | awk '{print $1}'` 
+	if [ "$pid"x == ""x ] && [ -f "/opt/kap-plus/sbin/neverStartFlag" ]
+	then 
+		if [ "$pid"x == ""x ] 
+		then 
+			echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - No running Kylin exits." 1>>$KYLINAPP_LOG  2>&1	
+	    fi
+	    
+		if [  -f "/opt/kap-plus/sbin/neverStartFlag" ] 
+		then 
+			echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - found neverStartFlag." 1>>$KYLINAPP_LOG  2>&1	
+	    fi
+	
+		rm /root/ignore_healthcheck
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - rm /root/ignore_healthcheck to recovery appcenter healthcheck. " 1>>$KYLINAPP_LOG  2>&1  
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - Action=$action End ...." 1>>$KYLINAPP_LOG  2>&1 
+		exit 0
+	fi
+	
+	  
+	echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - execute script:stop-kylin.sh." 1>>$KYLINAPP_LOG  2>&1  
 	/opt/kap-plus/sbin/stop-kylin.sh   
-	 
-	sleep 5
-	echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - sleep 5 seconds after stop kylin." 1>>$KYLINAPP_LOG  2>&1 
+	  
+	echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - Stop kylin then start." 1>>$KYLINAPP_LOG  2>&1 
 	
-	echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - start-kylin.sh." 1>>$KYLINAPP_LOG  2>&1 
-	
-	#echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - Test============================================================." 1>>$KYLINAPP_LOG  2>&1
-	#cat /opt/kap-plus/sbin/start-kylin.sh 1>>$KYLINAPP_LOG  2>&1
-	#echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - Test============================================================." 1>>$KYLINAPP_LOG  2>&1
-	
-	/opt/kap-plus/sbin/start-kylin.sh  
+	echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - execute script:j-start-kylin.sh." 1>>$KYLINAPP_LOG  2>&1   
+	/opt/kap-plus/sbin/j-start-kylin.sh  
 	if [  $? -ne 0 ] 
 	then 
-		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - Error -exit code=$?, Start Kylin Service failed." 1>>$KYLINAPP_LOG  2>&1	 
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - Error -exit code=$?, Start Kylin Service failed." 1>>$KYLINAPP_LOG  2>&1
+		rm /root/ignore_healthcheck
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - rm /root/ignore_healthcheck to recovery appcenter healthcheck. " 1>>$KYLINAPP_LOG  2>&1  
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - action-kylin.sh - INFO - Action=$action End ...." 1>>$KYLINAPP_LOG  2>&1	 
 		exit 1  
 	fi 
 	
-fi 
+fi  
 
 
 rm /root/ignore_healthcheck
--- scripts/kylin/sbin/kylinutil.sh
+++ scripts/kylin/sbin/kylinutil.sh
@@ -1,34 +1,53 @@
-#! /bin/bash
+#! /bin/bash 
+function checkHiveIsOK(){  
+	hivedefaultDB=$(/opt/hive/bin/hive -e "show databases;"|grep "default" |sed 's/ //g' )  
+	hiveIsOK='false' 
+	if [ "$hivedefaultDB"x == "default"x ] 
+	then 
+		hiveIsOK='true' 
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - checkHiveIsOK : HIVE is Runing!" 1>>$KYLINAPP_LOG  2>&1
+		echo $hiveIsOK
+		exit 0
+	fi 	 
+	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - checkHiveIsOK : HIVE is not Runing!" 1>>$KYLINAPP_LOG  2>&1   
+	echo $hiveIsOK 
+} 
 
-function checkHiveIsRunning(){   
-	for i in {1..500}
-	do
-		echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - Excute cmd:/opt/hive/bin/hive -e 'show databases;'" 1>>$KYLINAPP_LOG  2>&1 
-		/opt/hive/bin/hive -e "show databases;"  1>>$KYLINAPP_LOG  2>&1  
-		if [  $? -ne 0 ] 
+
+function waitHiveReady(){   
+	i=1   
+	while [ ${i} -le 50 ]  
+	do   	
+	  	#echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - Excute cmd:/opt/hive/bin/hive -e 'show databases;',to checkHiveIsOK." 1>>$KYLINAPP_LOG  2>&1  
+		hiveIsOK=$(checkHiveIsOK) 
+		#echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - waitHiveReady : hiveIsOK2=$hiveIsOK" 1>>$KYLINAPP_LOG  2>&1
+			
+		if [  "$hiveIsOK"x == "false"x ] 
 		then 
-			echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - checkHiveIsRunning:Waiting Hive to Start..." 1>>$KYLINAPP_LOG  2>&1
+			echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - waitHiveReady  : Waiting Hive to Start..." 1>>$KYLINAPP_LOG  2>&1
 			sleep 10
 		else
-			echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - checkHiveIsRunning:HIVE is Runing!" 1>>$KYLINAPP_LOG  2>&1
+			echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - waitHiveReady : HIVE is Runing!" 1>>$KYLINAPP_LOG  2>&1
 			break
-		fi 
-	done 
+		fi   
+	  	i=`expr ${i} + 1`
+	  	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - waitHiveReady : wait times=$i" 1>>$KYLINAPP_LOG  2>&1
+	done   
 	
-	/opt/hive/bin/hive -e "show databases;"  1>>$KYLINAPP_LOG  2>&1
-	if [  $? -ne 0 ] 
+	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - waitHiveReady : Jump wait" 1>>$KYLINAPP_LOG  2>&1
+	
+	hiveIsOK=$(checkHiveIsOK) 
+	if [  "$hiveIsOK"x == "false"x ]
 	then 
-		echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - Error - checkHiveIsRunning:Wait hive to start for 500s,but hive is still not running,kylin can not start." 1>>$KYLINAPP_LOG  2>&1  
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - Error - waitHiveReady : Wait hive to start for 500s,but hive is still not running,kylin can not start." 1>>$KYLINAPP_LOG  2>&1  
 		isRunning="false"
 	else
-		echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - checkHiveIsRunning:HIVE is Runing!" 1>>$KYLINAPP_LOG  2>&1
+		echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - waitHiveReady : HIVE is Runing!" 1>>$KYLINAPP_LOG  2>&1
 		isRunning="true"  
 	fi 
 	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - isRunning=$isRunning" 1>>$KYLINAPP_LOG  2>&1
 	echo $isRunning    
-}
-
- 
+} 
 
 function StartKAP(){   
 	USER=kylin /opt/kap-plus/bin/kylin.sh start  1>>$KYLINAPP_LOG  2>&1 
@@ -90,8 +109,7 @@ function DealWithHDFS4Kylin(){
 	hadoop fs -test -e /user
 	if [ $? -ne 0 ]
     then  
-    	hdfs dfs -mkdir /user
-    	hdfs dfs -chown kylin /user
+    	hdfs dfs -mkdir /user 
     	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - Create hdfs dir /user" 1>>$KYLINAPP_LOG  2>&1
 	fi 
 	
@@ -101,10 +119,7 @@ function DealWithHDFS4Kylin(){
     	hdfs dfs -mkdir /user/kylin
     	hdfs dfs -chown kylin /user/kylin
     	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - Create hdfs dir /user/kylin" 1>>$KYLINAPP_LOG  2>&1 
-	fi   
-	
-	hdfs dfs -chmod -R 777 /tmp
-	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - Chmod 777 hdfs dir /tmp" 1>>$KYLINAPP_LOG  2>&1 
+	fi    
 	
 	touch  /opt/kap-plus/sbin/hdfsfolder_created
 	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - DealWithHDFS4Kylin for kylin service init finished,create /opt/kap-plus/bin/sample_loaded Flag file." 1>>$KYLINAPP_LOG  2>&1
@@ -116,7 +131,7 @@ function DealWithHDFS4Kylin(){
 
 function loadSampleData4Kylin(){ 
 	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - Excute cmd: /opt/kap-plus/bin/sample.sh" 1>>$KYLINAPP_LOG  2>&1  
-	/opt/kap-plus/bin/sample.sh   1>>$KYLINAPP_LOG  2>&1   
+	/opt/kap-plus/bin/sample.sh    >/dev/null 2>&1
 	touch  /opt/kap-plus/sbin/sample_loaded
 	echo "`date '+%Y-%m-%d %H:%M:%S'` - kylinutil.sh - INFO - Load sample data for kylin service init finished,create /opt/kap-plus/bin/sample_loadedFlag file." 1>>$KYLINAPP_LOG  2>&1  
 	
